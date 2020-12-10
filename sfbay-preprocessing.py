29b82dba3984f6b0cb0154735a3b5e9a94beedf1
Peter Bognar <bognarpaeter@gmail.com>
2020-12-10 16:21:58 +0100

    changed field naming in pre-processing script

--- sfbay-preprocessing.py
+++ sfbay-preprocessing.py
@@ -49,27 +49,27 @@ locations = pd.read_csv(location_data_path, index_col=False)
 
 del locations["Comments"]
 #
-# stat_num: Station Number
+# Stations: Station Number
 # lat_deg: North Latitude Degrees
 # lat_min: North Latitude Minutes
 # lon_deg: West Longitude Degrees
 # lon_min: West Longitude Minutes
 #
-locations_header = ["stat_num", "lat_deg", "lat_min", "lon_deg", "lon_min"]
+locations_header = ["Stations", "lat_deg", "lat_min", "lon_deg", "lon_min"]
 locations.columns = locations_header
 
 # remove minute sign (') and cast to proper types
 locations["lat_min"] = locations["lat_min"].str.replace(r"'$", "")
 locations["lon_min"] = locations["lon_min"].str.replace(r"'$", "")
 locations = locations.astype("float64")
-locations["stat_num"] = locations["stat_num"].astype("int32")
+locations["Stations"] = locations["Stations"].astype("int32")
 
 # convert minutes to degrees and sum values
 locations["lat"] = locations["lat_deg"] + locations["lat_min"] / 60
 locations["lon"] = locations["lon_deg"] - locations["lon_min"] / 60
 
 # drop unnecesary columns
-columns_to_drop = [col for col in locations_header if col != "stat_num"]
+columns_to_drop = [col for col in locations_header if col != "Stations"]
 locations = locations.drop(columns_to_drop, axis=1)
 
 # Obtain list of mercator coordinates
@@ -88,12 +88,12 @@ locations = locations.drop(columns=["mercator"])
 # Pre-process SFBay data #
 #                        #
 
-sfbay.rename(columns={"Station.Number": "stat_num"}, inplace=True)
-sfbay["stat_num"] = sfbay["stat_num"].astype("int32")
+sfbay.rename(columns={"Station.Number": "Stations"}, inplace=True)
+sfbay["Stations"] = sfbay["Stations"].astype("int32")
 
 # sfbay_final = pd.concat([sfbay, locations], axis=1, join='inner')
 sfbay_final = sfbay.merge(
-    locations, on="stat_num", how="inner"
+    locations, on="Stations", how="inner"
 )  # , suffixes=('_1', '_2'))
 
 sfbay_final_data_path = os.path.join(DATA_PATH, sfbay_final_data)
